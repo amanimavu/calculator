e15b541b4230bd02ac1dc9898d3ace935aaa0585
Amani Mavu <amanimavu@gmail.com>
2024-12-25 00:09:09 +0300

    feat: improve the UX of the calculator
    1. Enable input via the keyboard
    2. Have a window that displays past evaluations

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const cells = document.getElementsByClassName("cell");
 const inputDisplay = document.getElementById("input");
 const expressionDisplay = document.getElementById("expression");
+const history = document.querySelector("#history-screen p");
+const sound = document.getElementById("sound");
+let resultArray = [];
+const maxHistory = 10;
+const operators = ["+", "-", "x", "/"];
 
 const calculate = (expression) => {
+    console.log(expression);
     expression = expression.split(" ");
     console.log(expression);
 
@@ -14,9 +20,10 @@ const calculate = (expression) => {
     // console.log(x, y, z)
 
     const priority = { x: 5, "/": 6, "+": 4, "-": 3 };
-    const operators = expression.filter(
-        (item) => item !== 0 && !parseInt(item)
+    const operators = expression.filter((item) =>
+        Object.keys(priority).includes(item)
     );
+
     const compareFn = (x, y) => {
         return priority[y] - priority[x];
     };
@@ -58,42 +65,188 @@ const calculate = (expression) => {
     return expression;
 };
 
+const erase = () => {
+    inputDisplay.textContent = "";
+    expressionDisplay.textContent = "";
+};
+
 function handleClick() {
     const input = this.innerText;
+    sound.currentTime = 0;
+    sound.play();
 
     const expression = expressionDisplay.innerText;
     switch (input) {
+        case "":
+            const inputText = inputDisplay.innerText;
+            const inputAfterErase = inputText.slice(0, inputText.length - 1);
+            inputDisplay.innerText = inputAfterErase || "\n";
+
+            let expressionAfterErase;
+            if (operators.includes(expression.at(-2))) {
+                expressionAfterErase = expression.slice(
+                    0,
+                    expression.length - 2
+                );
+            } else {
+                expressionAfterErase = expression.slice(
+                    0,
+                    expression.length - 1
+                );
+            }
+            expressionDisplay.textContent = expressionAfterErase;
+            break;
         case "=":
             if (!expression.includes("=")) {
-                const [result] = calculate(expression);
+                let [result] = calculate(expression);
+                if (!Number.isInteger(parseFloat(result))) {
+                    result = parseFloat(result).toFixed(2);
+                }
                 inputDisplay.textContent = result;
                 expressionDisplay.textContent += ` = ${result}`;
+
+                resultArray.unshift(expressionDisplay.textContent);
+                const expressions = resultArray.reduce((prev, current) => {
+                    return prev + "\n---------------\n" + current;
+                });
+                history.innerText = expressions;
+
+                if (resultArray.length === maxHistory) {
+                    resultArray = resultArray.slice(0, maxHistory - 1);
+                }
             }
             break;
         case "AC":
-            inputDisplay.textContent = "";
-            expressionDisplay.textContent = "";
+            erase();
             break;
         default:
             if (
-                !["+", "-", "x", "/"].includes(expression.at(-1)) &&
-                !["+", "-", "x", "/"].includes(input)
+                [
+                    "1",
+                    "2",
+                    "3",
+                    "4",
+                    "5",
+                    "6",
+                    "7",
+                    "8",
+                    "9",
+                    "0",
+                    "-",
+                    ".",
+                ].includes(expression.at(-1)) &&
+                !["+", "-", "x", "/"].includes(input) &&
+                !expression.includes("=")
             ) {
                 inputDisplay.textContent += input;
             } else {
                 inputDisplay.textContent = input;
             }
             if (!expression.includes("=")) {
-                if (["+", "-", "x", "/"].includes(input))
+                if (
+                    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].includes(
+                        expression.at(-1)
+                    ) &&
+                    ["+", "-", "x", "/"].includes(input)
+                )
                     expressionDisplay.textContent += ` ${input} `;
                 else expressionDisplay.textContent += input;
             } else {
-                const [prevResult] = expression.match(/(?<=\=\s*)-?\d+\.?\d*/);
-                expressionDisplay.textContent = `${prevResult} ${input} `;
+                if (["+", "-", "x", "/"].includes(input)) {
+                    const [prevResult] = expression.match(
+                        /(?<=\=\s*)-?\d+\.?\d*/
+                    );
+                    expressionDisplay.textContent = `${prevResult} ${input} `;
+                } else {
+                    expressionDisplay.textContent = input;
+                }
             }
     }
 }
 
 [...cells].forEach((cell) => {
     cell.addEventListener("click", handleClick);
+});
+
+$(document).ready(function () {
+    const grabArea = $("#grab-area");
+    const maxWidth = 480;
+    const minWidth = 20;
+    let open = false;
+
+    let mousedown;
+    let originalXPosition;
+
+    grabArea.one("mousedown", (event) => {
+        originalXPosition = event.clientX;
+    });
+
+    grabArea.on("dblclick", () => {
+        if (open) {
+            $("html").attr("style", `--translate-size: ${minWidth}px`);
+        } else {
+            $("html").attr("style", `--translate-size: ${maxWidth}px`);
+        }
+        open = !open;
+    });
+
+    grabArea.on("mousedown", (event) => {
+        mousedown = true;
+    });
+
+    $(this).on("mouseup", (event) => {
+        mousedown = false;
+    });
+
+    $(this).on("mousemove", (event) => {
+        if (mousedown) {
+            let offsetX = event.clientX - originalXPosition;
+
+            if (offsetX > maxWidth / 2) {
+                open = true;
+            } else {
+                open = false;
+            }
+
+            offsetX += minWidth;
+            if (offsetX > minWidth && offsetX < maxWidth) {
+                $("html").attr("style", `--translate-size: ${offsetX}px`);
+            }
+        }
+    });
+
+    $("#clear").on("click", () => {
+        resultArray = [];
+        history.innerText = "";
+    });
+
+    $(this).on("keypress", (event) => {
+        let keyCode = event.which;
+        if (keyCode === 61) {
+            keyCode = 13;
+        }
+        if (keyCode === 42) {
+            keyCode = 120;
+        }
+        $(`div[data-code=${keyCode}]`).trigger("click");
+    });
+
+    $(this).on("keydown", (event) => {
+        console.log(event.which);
+        const keyCode = event.which;
+        if (keyCode === 39) {
+            $("html").attr("style", `--translate-size: ${maxWidth}px`);
+        }
+        if (keyCode === 37) {
+            $("html").attr("style", `--translate-size: ${minWidth}px`);
+        }
+
+        if (keyCode === 8) {
+            $(`div[data-code=${keyCode}]`).trigger("click");
+        }
+
+        if (keyCode === 46) {
+            $("#clear").trigger("click");
+        }
+    });
 });
